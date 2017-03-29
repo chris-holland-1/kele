324cbcc8abc594131576c21f5f0a756eca9e62e0
Chris Holland <chris.holland987@gmail.com>
2017-03-29 16:39:49 -0700

    Checkpoint 2 Authorize: completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,12 +1,17 @@
-require 'rest_client'
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
 
   def initialize(email, password)
-    response = self.class.post("https://www.bloc.io/api/v1", header: {:content_type => 'application/json'}, body: {"email": email, "password": password})
-    raise Error.new() if response.code != 200
+    response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {"email": email, "password": password})
+    raise "Invalid email or password" if response.code != 200
     @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get("https://www.bloc.io/api/v1/users/me", headers: { "authorization" => @auth_token })
+    @current_user = JSON.parse(response.body)
   end
 end
--- lib/kele/error.rb
+++
@@ -1,5 +0,0 @@
-class Error
-  def initialize(msg='invalid email or password')
-    super(msg)
-  end
-end
