60ecfc0f9fe0c79fb257044a7eb8dabfd501048b
Chris Holland <chris.holland987@gmail.com>
2017-03-30 12:10:55 -0700

    Updated Kele file to Improve Error Code

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,9 +5,12 @@ class Kele
   include HTTParty
 
   def initialize(email, password)
-    response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {"email": email, "password": password})
-    raise "Invalid email or password" if response.code != 200
-    @auth_token = response["auth_token"]
+    #response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {"email": email, "password": password})
+    #raise "Invalid email or password" if response.code != 200
+    #@auth_token = response["auth_token"]
+    @api_url = 'https://www.bloc.io/api/v1'
+    @auth_token = self.class.post(@api_url + '/sessions', body: { email: username, password: password })["auth_token"]
+    raise StandardError.new('Invalid credentials') unless @auth_token
   end
 
   def get_me
